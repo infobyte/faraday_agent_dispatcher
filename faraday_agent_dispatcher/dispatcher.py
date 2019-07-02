76cee2e98490bdf02b628dbe3b82b5220d3fae0c
Eric Horvat <eric.nahuel.horvat@gmail.com>
2019-07-02 12:13:20 -0300

    [ADD] Logic of the run (in comments, to be implemented)

--- faraday_agent_dispatcher/dispatcher.py
+++ faraday_agent_dispatcher/dispatcher.py
@@ -3,6 +3,29 @@
 
 # TODO CONNECT INTERFACE
 
+class Dispatcher:
 
-def run():
-    pass
+    def connect(self):
+        # I'm built so I can connect
+
+        # Ask by API and Agent Token for WS token
+
+        # Turn on IPC
+
+        # Connect by WS
+
+        pass
+
+    def run(self):
+        # This must be called from ws listener
+
+        # Execute SH passed by config
+
+        pass
+
+    def send(self):
+        # Any time can be called by IPC
+
+        # Send by API and Agent Token the info
+        pass
+
