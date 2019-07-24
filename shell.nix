a64ae1caad8694d6f1e63bae91452ef907895c2e
Matías Lang <matiasl@infobytesec.com>
2019-07-24 14:09:48 -0300

    Add shell.nix file

---
+++ shell.nix
@@ -0,0 +1,30 @@
+with (import <nixpkgs> {});
+  mkShell {
+    buildInputs = (with python37Packages;
+      [virtualenv
+      ]);
+    shellHook = ''
+      unset SOURCE_DATE_EPOCH  # Required to make pip work
+
+      VENV_PATH=.venv
+
+      mkvirtualenv(){
+        # Reset previous virtualenv
+        type -t deactivate && deactivate
+        rm -rf $VENV_PATH
+
+        # Build new virtualenv with system packages
+        virtualenv --system-site-packages $VENV_PATH
+        source $VENV_PATH/bin/activate
+        python setup.py develop
+        pip install -r requirements_dev.txt
+      }
+
+      if [[ -d $VENV_PATH ]]; then
+        source $VENV_PATH/bin/activate
+      else
+        echo Creating new virtualenv
+        mkvirtualenv
+      fi
+    '';
+  }
