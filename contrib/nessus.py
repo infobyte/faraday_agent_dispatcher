057c2039ee44a5bb7c8ca113bb462adf8c5f2eab
dnadares <dnadares@infobytesec.com>
2019-12-23 11:35:07 -0300

    No general exceptions

--- contrib/nessus.py
+++ contrib/nessus.py
@@ -20,14 +20,11 @@ def nessus_login(url, user, password):
 
 def nessus_templates(url, token, x_token = '', target = ''):
     headers = {'X-Cookie': 'token={}'.format(token), 'X-API-Token': x_token}
-    try:
-        payload = { }
-        r = requests.get(url + '/editor/scan/templates', json=payload, headers=headers, verify=False).json()
-        return { t['name']: t['uuid'] for t in r['templates']}
-    except Exception as e:
-        logging.error (e)
 
-    return
+    payload = { }
+    r = requests.get(url + '/editor/scan/templates', json=payload, headers=headers, verify=False).json()
+
+    return { t['name']: t['uuid'] for t in r['templates']}
 
 
 def nessus_add_target(url, token, x_token = '', target = '', template = 'basic', name = 'nessus_scan'):
@@ -38,65 +35,62 @@ def nessus_add_target(url, token, x_token = '', target = '', template = 'basic',
         logging.error ('Template {} not valid. Setting basic as default.'.format(template))
         template = 'basic'
 
-    try:
-        payload = { 'uuid': '{}'.format(templates[template]), 'settings': { 'name': '{}'.format(name), 'enabled': True, 'text_targets': target, 'agent_group_id': [] }}
-        r = requests.post(url + '/scans', json=payload, headers=headers, verify=False).json()
-        return r['scan']['id']
-    except Exception as e:
-        logging.error (e)
+    payload = { 'uuid': '{}'.format(templates[template]), 'settings': { 'name': '{}'.format(name), 'enabled': True, 'text_targets': target, 'agent_group_id': [] }}
+    r = requests.post(url + '/scans', json=payload, headers=headers, verify=False).json()
+
+    return r['scan']['id']
 
 
 def nessus_scan_run(url, scan_id, token, x_token = '', target = 'basic', policies=''):
     headers = {'X-Cookie': 'token={}'.format(token), 'X-API-Token': x_token }
-    try:
-        s = requests.post(url + '/scans/{scan_id}/launch'.format(scan_id=scan_id), headers=headers, verify=False).json()
-        logging.error ('scan', s)
-        scan_uuid = s['scan_uuid']
 
-        read = { 'read': False }
+    s = requests.post(url + '/scans/{scan_id}/launch'.format(scan_id=scan_id), headers=headers, verify=False).json()
+    logging.error ('scan', s)
+    scan_uuid = s['scan_uuid']
+
+    read = { 'read': False }
+    s = requests.get(url + '/scans/{scan_id}'.format(scan_id=scan_id), headers=headers, verify=False)
+    #logging.error("scans ", s.json())
+    status = s.json()['info']['status']
+    while status == 'running':
+        logging.error("scan status check ", s)
+        time.sleep(5)
         s = requests.get(url + '/scans/{scan_id}'.format(scan_id=scan_id), headers=headers, verify=False)
-        #logging.error("scans ", s.json())
         status = s.json()['info']['status']
-        while status == 'running':
-            logging.error("scan status check ", s)
-            time.sleep(5)
-            s = requests.get(url + '/scans/{scan_id}'.format(scan_id=scan_id), headers=headers, verify=False)
-            status = s.json()['info']['status']
-        logging.error("scan status", status)
-    except Exception as e:
-        logging.error (e)
+    logging.error("scan status", status)
 
 def nessus_scan_export(url, scan_id, token, x_token = '', target = ''):
+    content = None
     headers = {'X-Cookie': 'token={}'.format(token), 'X-API-Token': x_token}
-    try:
-        r = requests.post(url + '/scans/{scan_id}/export?limit=2500'.format(scan_id=scan_id), data={'format': 'nessus'}, headers=headers, verify=False).json()
 
-        if r['token']:
+    r = requests.post(url + '/scans/{scan_id}/export?limit=2500'.format(scan_id=scan_id), data={'format': 'nessus'}, headers=headers, verify=False).json()
+
+    if r['token']:
+        s = requests.get(url + '/tokens/{token}/status'.format(token = r['token']), verify=False).json()
+        status = s['status']
+        while status != 'ready':
+            logging.error("report export status check ", s)
+            time.sleep(5)
             s = requests.get(url + '/tokens/{token}/status'.format(token = r['token']), verify=False).json()
             status = s['status']
-            while status != 'ready':
-                logging.error("report export status check ", s)
-                time.sleep(5)
-                s = requests.get(url + '/tokens/{token}/status'.format(token = r['token']), verify=False).json()
-                status = s['status']
-            logging.error ('report export status ', status)
-            r = requests.get(url + '/tokens/{token}/download'.format(token = r['token']), allow_redirects=True, verify=False)
-            return r.content
-    except Exception as e:
-        logging.error (e)
+        logging.error ('report export status ', status)
+        r = requests.get(url + '/tokens/{token}/download'.format(token = r['token']), allow_redirects=True, verify=False)
+        content = r.content
+
+    return content
 
 def get_x_api_token(url, token):
+    x_token = None
     headers = {'X-Cookie': 'token={}'.format(token)} 
+
     pattern = r"\{key:\"getApiToken\",value:function\(\)\{return\"([a-zA-Z0-9]*-[a-zA-Z0-9]*-[a-zA-Z0-9]*-[a-zA-Z0-9]*-[a-zA-Z0-9]*)\"\}"
-    try:
-        r = requests.get(url + '/nessus6.js', headers=headers, verify=False)
-        content = str(r.content)
-        matched = re.search(pattern, str(r.content))
-        if matched:
-            x_token = matched.group(1)
-    except Exception as e:
-        x_token = None
-        logging.error(e)
+    r = requests.get(url + '/nessus6.js', headers=headers, verify=False)
+
+    content = str(r.content)
+    matched = re.search(pattern, str(r.content))
+
+    if matched:
+        x_token = matched.group(1)
 
     return x_token
 
