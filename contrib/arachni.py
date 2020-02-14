3e3af29473cf3955ff1ab15965de52d4f6087f9c
Blas <bmoyano@infobytesec.com>
2020-02-14 15:23:07 -0300

    Arachni url for params

--- contrib/arachni.py
+++ contrib/arachni.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 import os
-import sys
 from faraday_plugins.plugins.manager import PluginsManager
 
 
-def get_arachni_path_xml(path):
+def get_arachni_path_xml(path, url_analyze):
     os.chdir(path)
-    url = 'https://google.com'
     name_result = 'report.afr'
     xml_result = 'xml_arachni_report.xml'
-    os.system("./arachni {} --report-save-path={}".format(url, name_result))
+    os.system("./arachni {} --report-save-path={}".format(url_analyze, name_result))
     os.system("./arachni_reporter {} --reporter=xml:outfile={}".format(name_result, xml_result))
     path_xml = os.path.join(os.getcwd(), xml_result)
     return path_xml
 
 
-def main(data: str = None):
+def main():
+    url_analyze = os.environ.get('EXECUTOR_CONFIG_NOMBRE_URL')
     path = os.path.join(os.path.expanduser('~'), 'Descargas', 'arachni-1.5.1-0.5.12', 'bin')
-    xml_arachni = get_arachni_path_xml(path)
+    xml_arachni = get_arachni_path_xml(path, url_analyze)
     plugin = PluginsManager().get_plugin("arachni")
     plugin.parseOutputString(xml_arachni)
     print(plugin.get_json())
@@ -25,3 +24,4 @@ def main(data: str = None):
 
 if __name__ == '__main__':
     main()
+
