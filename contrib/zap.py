35e4ffb0bef7d26d654dabfb2e55ddee91d7cd52
Blas <bmoyano@infobytesec.com>
2020-04-28 23:01:52 -0300

    use tempfile, valid env

--- contrib/zap.py
+++ contrib/zap.py
@@ -5,19 +5,7 @@ from faraday_plugins.plugins.manager import PluginsManager
 import time
 from zapv2 import ZAPv2
 import subprocess
-
-
-def report_zap(target, api_key, dir_save):
-    zap = ZAPv2(apikey=api_key)
-    scanID = zap.spider.scan(target)
-    while int(zap.spider.status(scanID)) < 100:
-        time.sleep(1)
-
-    file = open(dir_save, 'w')
-    file.write(zap.core.xmlreport())
-    file.close()
-    zap.core.shutdown()
-    return dir_save
+import tempfile
 
 
 def run_zap_proxy(path):
@@ -26,21 +14,39 @@ def run_zap_proxy(path):
 
 
 def main():
-    target = os.environ.get('EXECUTOR_CONFIG_TARGET_URL')
-    api_key = os.environ.get('API_KEY')
-    my_env = os.environ
-    dir_save = '/home/blas/Escritorio/report.xml'
-    path = my_env["ZAP"] if 'ZAP' in my_env else "/home/blas/Descargas/ZAP_2.9.0/zap.sh"
-    pid_zap = run_zap_proxy(path)
-    if pid_zap == 0:
-        print("Zap Proxy not found", file=sys.stderr)
-    else:
-        time.sleep(30)
-        zap_xml = report_zap(target, api_key, dir_save)
+    try:
+        target = os.environ.get('EXECUTOR_CONFIG_TARGET_URL')
+        api_key = os.environ.get('API_KEY')
+    except KeyError:
+        print("environment variable not found", file=sys.stderr)
+        sys.exit()
 
-    plugin = PluginsManager().get_plugin("zap")
-    plugin.parseOutputString(zap_xml)
-    print(plugin.get_json())
+    my_env = os.environ
+    if 'ZAP' in my_env:
+        path = my_env["ZAP"]
+        pid_zap = run_zap_proxy(path)
+
+        if pid_zap == 0:
+            print("Zap Proxy not found", file=sys.stderr)
+            sys.exit()
+        else:
+            time.sleep(30)
+            zap = ZAPv2(apikey=api_key)
+            scanID = zap.spider.scan(target)
+            while int(zap.spider.status(scanID)) < 100:
+                time.sleep(1)
+
+            ft = tempfile.TemporaryFile('w+t')
+            ft.write(zap.core.xmlreport())
+            ft.seek(0)
+            zap.core.shutdown()
+            plugin = PluginsManager().get_plugin("zap")
+            plugin.parseOutputString(ft.read())
+            ft.close()
+            print(plugin.get_json())
+    else:
+        print("ZAP environment variable not found", file=sys.stderr)
+        sys.exit()
 
 
 if __name__ == '__main__':
