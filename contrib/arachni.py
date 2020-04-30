aeba3d10ea678bf19ea812ed13598d12913aa0a9
Blas <bmoyano@infobytesec.com>
2020-04-30 16:30:21 -0300

    fix arachni enviroment

--- contrib/arachni.py
+++ contrib/arachni.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python
 import os
 from faraday_plugins.plugins.manager import PluginsManager
+import sys
+import tempfile
+import time
 
-
-def get_arachni_path_xml(path, url_analyze):
-    os.chdir(path)
-    name_result = 'report.afr'
-    xml_result = 'xml_arachni_report.xml'
-    os.system("./arachni {} --report-save-path={}".format(url_analyze, name_result))
-    os.system("./arachni_reporter {} --reporter=xml:outfile={}".format(name_result, xml_result))
-    path_xml = os.path.join(os.getcwd(), xml_result)
-    return path_xml
 
 
 def main():
-    url_analyze = os.environ.get('EXECUTOR_CONFIG_NOMBRE_URL')
-    path = os.path.join(os.path.expanduser('~'), 'Descargas', 'arachni-1.5.1-0.5.12', 'bin')
-    xml_arachni = get_arachni_path_xml(path, url_analyze)
-    plugin = PluginsManager().get_plugin("arachni")
-    plugin.parseOutputString(xml_arachni)
-    print(plugin.get_json())
+    my_envs = os.environ
+    if 'EXECUTOR_CONFIG_NAME_URL' in my_envs:
+        url_analyze = os.environ.get('EXECUTOR_CONFIG_NAME_URL')
+    else:
+        print("Environment variables no set", file=sys.stderr)
+        sys.exit()
+
+    if 'ARACHNI' in my_envs:
+        path_arachni = os.environ.get('ARACHNI')
+    else:
+        print("Environment variables no set", file=sys.stderr)
+        sys.exit()
+
+    os.chdir(path_arachni)
+    with tempfile.TemporaryDirectory() as tempdirname:
+        name_result = f'{tempdirname}/report.afr'
+        xml_result = f'{tempdirname}/xml_arachni_report.xml'
+        os.system(f'./arachni {url_analyze} --report-save-path={name_result}')
+        os.system(f'./arachni_reporter {name_result} --reporter=xml:outfile={xml_result}')
+        plugin = PluginsManager().get_plugin("arachni")
+        plugin.parseOutputString(xml_result)
+        print(plugin.get_json())
 
 
 if __name__ == '__main__':
