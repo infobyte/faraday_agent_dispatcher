887e40a7e0a768a5bd497807b1ac5885c65929f7
Blas <bmoyano@infobytesec.com>
2020-05-01 20:34:32 -0300

    manifest, change w3af

--- faraday_agent_dispatcher/static/executors/official/w3af.py
+++ faraday_agent_dispatcher/static/executors/official/w3af.py
@@ -2,37 +2,42 @@
 import os
 import sys
 from faraday_plugins.plugins.manager import PluginsManager
-import time
 import subprocess
-
-
-def create_w3af_file(url_targe, path_file_w3af):
-    text = "" \
-           "plugins\n output console,xml_file\n output\n output config xml_file\n set output_file output-w3af.xml\n " \
-           "set verbose True\n back\n output config console\n set verbose False\n back\n " \
-           "crawl all, !bing_spider, !google_spider, !spider_man\n crawl\n grep all\n grep\n audit all\n audit\n " \
-           "bruteforce all\n bruteforce\n back\n target\n set target {}\n back\n start\n exit".format(url_targe)
-    file = open(path_file_w3af, "w")
-    file.write(text)
-    file.close()
-    return path_file_w3af
-
-
-def get_path_xml():
-    path = os.path.join(os.path.expanduser('~'), 'Desarrollo', 'w3af')
-    path_xml = "{}/output-w3af.xml".format(path)
-    return path_xml
+import tempfile
 
 
 def main():
-    url_target = os.environ.get('EXECUTOR_CONFIG_TARGET_URL')
-    path_file_w3af = "/home/blas/Desarrollos/faraday_agent_dispatcher/contrib/config_report_file.w3af"
-    create_w3af_file(url_target, path_file_w3af)
-    subprocess.call("/home/blas/Desarrollos/faraday_agent_dispatcher/contrib/run_w3af.sh")
-    xml = get_path_xml()
-    plugin = PluginsManager().get_plugin("W3af")
-    plugin.parseOutputString(xml)
-    print(plugin.get_json())
+    url_target = os.environ.get('EXECUTOR_CONFIG_W3AF_TARGET_URL')
+    if not url_target:
+        print("URL not provided", file=sys.stderr)
+        sys.exit()
+
+    if 'W3AF_PATH' in os.environ:
+        with tempfile.TemporaryDirectory() as tempdirname:
+            name_result = f'{tempdirname}/config_report_file.w3af'
+            file_w3af = open(name_result, "w")
+            command_text = "" \
+                           "plugins\n output console,xml_file\n output\n output config xml_file\n " \
+                           "set output_file {}/output-w3af.xml\n set verbose True\n back\n output config console\n " \
+                           "set verbose False\n back\n crawl all, !bing_spider, !google_spider, !spider_man\n crawl\n" \
+                           " grep all\n grep\n audit all\n audit\n bruteforce all\n bruteforce\n back\n target\n" \
+                           " set target {}\n back\n start\n exit".format(tempdirname, url_target)
+            file_w3af.write(command_text)
+            file_w3af.close()
+            try:
+                os.chdir(path=os.environ.get('W3AF_PATH'))
+            except FileNotFoundError:
+                print("No such file or directory", file=sys.stderr)
+                sys.exit()
+
+            subprocess.run(f'./w3af_console -s {name_result}', shell=True)
+            plugin = PluginsManager().get_plugin("w3af")
+            plugin.parseOutputString(f'{tempdirname}/output-w3af.xml')
+            print(plugin.get_json())
+
+    else:
+        print("W3AF_PATH not set", file=sys.stderr)
+        sys.exit()
 
 
 if __name__ == '__main__':
