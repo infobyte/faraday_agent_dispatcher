cf54a144995bafe43a78b551e78ce9fc1196863b
Blas <bmoyano@infobytesec.com>
2020-05-01 15:39:05 -0300

    merge tkt 45

--- contrib/run_w3af.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-source /home/blas/.virtualenvs/w3af/bin/activate
-cd /home/blas/Desarrollos/w3af
-exec ./w3af_console -s /home/blas/Desarrollos/faraday_agent_dispatcher/contrib/config_report_file.w3af
