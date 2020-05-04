d608b0277552536079b39f4d1f166f20a42872dc
Blas <bmoyano@infobytesec.com>
2020-05-04 17:53:33 -0300

    nikto manifest and nikto executor

---
+++ faraday_agent_dispatcher/static/executors/official/nikto2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import sys
+from faraday_plugins.plugins.repo.nikto.plugin import NiktoPlugin
+import subprocess
+import tempfile
+import time
+
+def main():
+    url_target = os.environ.get('EXECUTOR_CONFIG_NIKTO2_TARGET_URL')
+    url_port = os.environ.get('EXECUTOR_CONFIG_NIKTO2_TARGET_PORT')
+    if not url_target:
+        print("URL not provided", file=sys.stderr)
+        sys.exit()
+
+    with tempfile.TemporaryDirectory() as tempdirname:
+        name_result = f'{tempdirname}/output.xml'
+        if not url_port:
+            command = f'nikto -h {url_target} -o {name_result}'
+        else:
+            command = f'nikto -h {url_target} -p {url_port} -o {name_result}'
+
+        subprocess.run(command, shell=True)
+        plugin = NiktoPlugin()
+        f = open(name_result, 'r')
+        f.seek(0)
+        plugin.parseOutputString(f.read())
+        print(plugin.get_json())
+
+
+if __name__ == '__main__':
+    main()
