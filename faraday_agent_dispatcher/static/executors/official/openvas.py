6103b1e2a579484c4520542253ac25c4103eddbb
Blas <bmoyano@infobytesec.com>
2020-05-08 11:54:09 -0300

    init openvas executor

---
+++ faraday_agent_dispatcher/static/executors/official/openvas.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+import sys
+from faraday_plugins.plugins.repo.wpscan.plugin import WPScanPlugin
+import subprocess
+import tempfile
+from subprocess import Popen, PIPE
+
+
+def main():
+    pass
+    # Imagen Docker
+    # https://hub.docker.com/r/mikesplain/openvas
+    # docker pull mikesplain/openvas
+
+
+if __name__ == '__main__':
+    main()
