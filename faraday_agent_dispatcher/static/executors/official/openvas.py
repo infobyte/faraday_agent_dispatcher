8dda0231f5a6de6fc2b7a2442278f3da90b94b3d
Blas <bmoyano@infobytesec.com>
2020-05-09 01:27:10 -0300

    openvas openvas_manifest

--- faraday_agent_dispatcher/static/executors/official/openvas.py
+++ faraday_agent_dispatcher/static/executors/official/openvas.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python
 import os
 import sys
-from faraday_plugins.plugins.repo.wpscan.plugin import WPScanPlugin
+from faraday_plugins.plugins.repo.openvas.plugin import OpenvasPlugin
 import subprocess
 import tempfile
 from subprocess import Popen, PIPE
+from pathlib import Path
 
 
 def main():
-    pass
     # Imagen Docker
-    # https://hub.docker.com/r/mikesplain/openvas
-    # docker pull mikesplain/openvas
+    # https://hub.docker.com/r/ictu/openvas-docker
+    # docker run --rm -v $(pwd):/openvas/results/ ictu/openvas-docker /openvas/run_scan.py 123.123.123.123,www.github.com openvas_scan_report
+
+    url_target = os.environ.get('EXECUTOR_CONFIG_OPENVAS_TARGET_URL')
+    url_target = 'www.github.com'
+    if not url_target:
+        print("git URL not provided", file=sys.stderr)
+        sys.exit()
+
+    p = Popen(['sudo', 'docker', 'images'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+    if str(output).find('ictu/openvas-docker') >= 0:
+        with tempfile.TemporaryDirectory() as tempdirname:
+            command = f'docker run --rm -v {tempdirname}:/openvas/results/ ictu/openvas-docker ' \
+                      f'/openvas/run_scan.py 123.123.123.123,{url_target} openvas_scan_report'
+            subprocess.run(command, shell=True)
+            plugin = OpenvasPlugin()
+            name_xml = Path(tempdirname) / 'openvas_scan_report.xml'
+            if name_xml.exists():
+                f = open(name_xml, 'r')
+                f.seek(0)
+                plugin.parseOutputString(f.read())
+                print(plugin.get_json())
+            else:
+                print("File no exists", file=sys.stderr)
+                sys.exit()
+
+    else:
+        print("Docker image is not found", file=sys.stderr)
+        sys.exit()
 
 
 if __name__ == '__main__':
