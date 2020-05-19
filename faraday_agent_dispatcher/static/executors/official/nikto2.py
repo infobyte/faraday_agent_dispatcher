01cd502c7ee3e0830a0a1cb5bb4e4c49ab18f1ee
Blas <bmoyano@infobytesec.com>
2020-05-19 13:43:11 -0300

    chenge open file, if in stdout | stderr

--- faraday_agent_dispatcher/static/executors/official/nikto2.py
+++ faraday_agent_dispatcher/static/executors/official/nikto2.py
@@ -11,6 +11,8 @@ def main():
     # ['EXECUTOR_CONFIG_TARGET_URL', 'EXECUTOR_CONFIG_TARGET_PORT']
     url_target = os.environ.get('EXECUTOR_CONFIG_TARGET_URL')
     url_port = os.environ.get('EXECUTOR_CONFIG_TARGET_PORT')
+    url_target = 'localhost'
+    url_port = 8080
     if not url_target:
         print("URL not provided", file=sys.stderr)
         sys.exit()
@@ -23,13 +25,14 @@ def main():
             command = f'nikto -h {url_target} -p {url_port} -o {name_result}'
 
         nikto_process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(f"Nikto stdout: {nikto_process.stdout.decode('utf-8')}", file=sys.stderr)
-        print(f"Nikto stderr: {nikto_process.stderr.decode('utf-8')}", file=sys.stderr)
+        if len(nikto_process.stdout) > 0:
+            print(f"Nikto stdout: {nikto_process.stdout.decode('utf-8')}", file=sys.stderr)
+        if len(nikto_process.stderr) > 0:
+            print(f"Nikto stderr: {nikto_process.stderr.decode('utf-8')}", file=sys.stderr)
         plugin = NiktoPlugin()
-        f = open(name_result, 'r')
-        f.seek(0)
-        plugin.parseOutputString(f.read())
-        print(plugin.get_json())
+        with open(name_result, 'r') as f:
+            plugin.parseOutputString(f.read())
+            print(plugin.get_json())
 
 
 if __name__ == '__main__':
