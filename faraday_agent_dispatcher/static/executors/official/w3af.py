dae058a4807c7192361cc41d1c34dd26139f8707
Blas <bmoyano@infobytesec.com>
2020-05-19 14:30:34 -0300

    change open for with open; print stdout, stderr

--- faraday_agent_dispatcher/static/executors/official/w3af.py
+++ faraday_agent_dispatcher/static/executors/official/w3af.py
@@ -18,16 +18,16 @@ def main():
     if 'W3AF_PATH' in os.environ:
         with tempfile.TemporaryDirectory() as tempdirname:
             name_result = Path(tempdirname) / 'config_report_file.w3af'
-            file_w3af = open(name_result, "w")
-            command_text = f'plugins\n output console,xml_file\n output\n output config xml_file\n ' \
-                           f'set output_file {tempdirname}/output-w3af.xml\n set verbose True\n back\n ' \
-                           f'output config console\n set verbose False\n back\n crawl all, !bing_spider, ' \
-                           f'!google_spider, !spider_man\n crawl\n grep all\n grep\n audit all\n audit\n ' \
-                           f'bruteforce all\n bruteforce\n back\n target\n set target {url_target}\n ' \
-                           f'back\n start\n exit'
 
-            file_w3af.write(command_text)
-            file_w3af.close()
+            with open(name_result, 'w') as f:
+                command_text = f'plugins\n output console,xml_file\n output\n output config xml_file\n ' \
+                               f'set output_file {tempdirname}/output-w3af.xml\n set verbose True\n back\n ' \
+                               f'output config console\n set verbose False\n back\n crawl all, !bing_spider, ' \
+                               f'!google_spider, !spider_man\n crawl\n grep all\n grep\n audit all\n audit\n ' \
+                               f'bruteforce all\n bruteforce\n back\n target\n set target {url_target}\n ' \
+                               f'back\n start\n exit'
+                f.write(command_text)
+
             try:
                 os.chdir(path=os.environ.get('W3AF_PATH'))
 
@@ -37,7 +37,13 @@ def main():
                 sys.exit()
 
             if os.path.isfile('w3af_console'):
-                subprocess.run(f'./w3af_console -s {name_result}', shell=True)
+                w3af_process = subprocess.run(f'./w3af_console -s {name_result}', shell=True, stdout=subprocess.PIPE,
+                                              stderr=subprocess.PIPE)
+                if len(w3af_process.stdout) > 0:
+                    print(f"W3AF stdout: {w3af_process.stdout.decode('utf-8')}", file=sys.stderr)
+                if len(w3af_process.stderr) > 0:
+                    print(f"W3AF stderr: {w3af_process.stderr.decode('utf-8')}", file=sys.stderr)
+
                 plugin = PluginsManager().get_plugin("w3af")
                 plugin.parseOutputString(f'{tempdirname}/output-w3af.xml')
                 print(plugin.get_json())
