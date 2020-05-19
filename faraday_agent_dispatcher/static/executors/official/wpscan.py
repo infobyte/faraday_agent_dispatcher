7b0322f86455f0862a7d5761008a704512aa0321
Blas <bmoyano@infobytesec.com>
2020-05-19 13:55:52 -0300

    open file, if check docker images delete

--- faraday_agent_dispatcher/static/executors/official/wpscan.py
+++ faraday_agent_dispatcher/static/executors/official/wpscan.py
@@ -4,7 +4,6 @@ import sys
 from faraday_plugins.plugins.repo.wpscan.plugin import WPScanPlugin
 import subprocess
 import tempfile
-from subprocess import Popen, PIPE
 
 
 def main():
@@ -15,26 +14,21 @@ def main():
         print("URL not provided", file=sys.stderr)
         sys.exit()
 
-    p = Popen(['docker', 'images'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-    if str(output).find('wpscanteam/wpscan') >= 0:
-        with tempfile.TemporaryDirectory() as tempdirname:
-            name_output_file = 'wpscan-output.json'
-            command = f'docker run --rm --mount type=bind,source={tempdirname},target=/output ' \
-                      f'wpscanteam/wpscan:latest -o /output/{name_output_file} --url {url_target} -f json'
+    with tempfile.TemporaryDirectory() as tempdirname:
+        name_output_file = 'wpscan-output.json'
+        command = f'docker run --rm --mount type=bind,source={tempdirname},target=/output ' \
+                  f'wpscanteam/wpscan:latest -o /output/{name_output_file} --url {url_target} -f json'
 
-            wpscan_proccess = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        wpscan_proccess = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if len(wpscan_proccess.stdout) > 0:
             print(f"Wpscan stdout: {wpscan_proccess.stdout.decode('utf-8')}", file=sys.stderr)
+        if len(wpscan_proccess.stderr) > 0:
             print(f"Wpscan stderr: {wpscan_proccess.stderr.decode('utf-8')}", file=sys.stderr)
 
-            plugin = WPScanPlugin()
-            f = open(f'{tempdirname}/{name_output_file}', 'r')
-            f.seek(0)
+        plugin = WPScanPlugin()
+        with open(f'{tempdirname}/{name_output_file}', 'r') as f:
             plugin.parseOutputString(f.read())
             print(plugin.get_json())
-    else:
-        print("Docker image is not found", file=sys.stderr)
-        sys.exit()
 
 
 if __name__ == '__main__':
