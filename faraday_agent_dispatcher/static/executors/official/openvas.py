697bee341fbe20327ce66b07e6e2fd1c0e0d0d81
Blas <bmoyano@infobytesec.com>
2020-05-19 18:45:46 -0300

    print stderr, stdout. Delete validate image docker

--- faraday_agent_dispatcher/static/executors/official/openvas.py
+++ faraday_agent_dispatcher/static/executors/official/openvas.py
@@ -4,39 +4,37 @@ import sys
 from faraday_plugins.plugins.repo.openvas.plugin import OpenvasPlugin
 import subprocess
 import tempfile
-from subprocess import Popen, PIPE
 from pathlib import Path
 
 
 def main():
     # If the script is run outside the dispatcher the environment variables are checked.
     # ['EXECUTOR_CONFIG_OPENVAS_SCAN_NAME']
-    url_target = os.environ.get('EXECUTOR_CONFIG_OPENVAS_SCAN_NAME')
+    #url_target = os.environ.get('EXECUTOR_CONFIG_OPENVAS_SCAN_NAME')
+    url_target = 'http://pidgr.com/'
     if not url_target:
         print("URL not provided", file=sys.stderr)
         sys.exit()
 
-    p = Popen(['sudo', 'docker', 'images'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-    if str(output).find('ictu/openvas-docker') >= 0:
-        with tempfile.TemporaryDirectory() as tempdirname:
-            command = f'docker run --rm -v {tempdirname}:/openvas/results/ ictu/openvas-docker ' \
-                      f'/openvas/run_scan.py 123.123.123.123,{url_target} openvas_scan_report'
-            subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            plugin = OpenvasPlugin()
-            name_xml = Path(tempdirname) / 'openvas_scan_report.xml'
-            if name_xml.exists():
-                f = open(name_xml, 'r')
-                f.seek(0)
+    with tempfile.TemporaryDirectory() as tempdirname:
+        tempdir = Path(tempdirname)
+        command = f'sudo docker run --rm -v $(pwd):/openvas/results/ ictu/openvas-docker ' \
+                  f'/openvas/run_scan.py 123.123.123.123,{url_target} openvas_scan_report'
+
+        openvas_process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if len(openvas_process.stdout) > 0:
+            print(f"Openvas stdout: {openvas_process.stdout.decode('utf-8')}", file=sys.stderr)
+        if len(openvas_process.stderr) > 0:
+            print(f"Openvas stderr: {openvas_process.stderr.decode('utf-8')}", file=sys.stderr)
+        plugin = OpenvasPlugin()
+        name_xml = tempdir / 'openvas_scan_report.xml'
+        if name_xml.exists():
+            with open(name_xml, 'r') as f:
                 plugin.parseOutputString(f.read())
                 print(plugin.get_json())
-            else:
-                print("File no exists", file=sys.stderr)
-                sys.exit()
-
-    else:
-        print("Docker image is not found", file=sys.stderr)
-        sys.exit()
+        else:
+            print("File no exists", file=sys.stderr)
+            sys.exit()
 
 
 if __name__ == '__main__':
