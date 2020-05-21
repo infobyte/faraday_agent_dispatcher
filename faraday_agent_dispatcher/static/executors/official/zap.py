76c1aee201fec2927341ef3d3f6ed72df743a87d
Eric Horvat <eric.nahuel.horvat@gmail.com>
2020-05-21 15:03:57 -0300

    [DEL] Remove executing zap

--- faraday_agent_dispatcher/static/executors/official/zap.py
+++ faraday_agent_dispatcher/static/executors/official/zap.py
@@ -4,48 +4,33 @@ import sys
 from faraday_plugins.plugins.manager import PluginsManager
 import time
 from zapv2 import ZAPv2
-import subprocess
 import tempfile
-
-
-def run_zap_proxy(path):
-    pid = subprocess.Popen(path, shell=True).pid
-    return pid
 
 
 def main():
     # If the script is run outside the dispatcher the environment variables are checked.
     # ['API_KEY', 'EXECUTOR_CONFIG_TARGET_URL', 'ZAP_PATH']
     try:
-        target = os.environ.get('EXECUTOR_CONFIG_TARGET_URL')
-        api_key = os.environ.get('API_KEY')
+        target = os.environ['EXECUTOR_CONFIG_TARGET_URL']
+        api_key = os.environ['API_KEY']
     except KeyError:
         print("environment variable not found", file=sys.stderr)
         sys.exit()
 
+    # Chequear que este levantado, sino exitear de manera feliz
+
     my_env = os.environ
-    if 'ZAP_PATH' in my_env:
-        path = my_env["ZAP_PATH"]
-        pid_zap = run_zap_proxy(path)
+    zap = ZAPv2(apikey=api_key)
+    scanID = zap.spider.scan(target)
+    while int(zap.spider.status(scanID)) < 100:
+        time.sleep(1)
 
-        if pid_zap == 0:
-            print("Zap Proxy not found", file=sys.stderr)
-            sys.exit()
-        else:
-            zap = ZAPv2(apikey=api_key)
-            scanID = zap.spider.scan(target)
-            while int(zap.spider.status(scanID)) < 100:
-                time.sleep(1)
-
-            with tempfile.TemporaryFile('w+t') as ft:
-                ft.write(zap.core.xmlreport())
-                zap.core.shutdown()
-                plugin = PluginsManager().get_plugin("zap")
-                plugin.parseOutputString(ft.read())
-                print(plugin.get_json())
-    else:
-        print("ZAP environment variable not found", file=sys.stderr)
-        sys.exit()
+    with tempfile.TemporaryFile('w+t') as ft:
+        ft.write(zap.core.xmlreport())
+        zap.core.shutdown()
+        plugin = PluginsManager().get_plugin("zap")
+        plugin.parseOutputString(ft.read())
+        print(plugin.get_json())
 
 
 if __name__ == '__main__':
