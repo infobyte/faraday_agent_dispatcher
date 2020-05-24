e254052d6b344a322c225e9d70fd30118c461793
Blas <bmoyano@infobytesec.com>
2020-05-23 22:44:23 -0300

    fix for agent arachni not save xml

--- faraday_agent_dispatcher/static/executors/official/arachni.py
+++ faraday_agent_dispatcher/static/executors/official/arachni.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-import tempfile
 import subprocess
 from pathlib import Path
 from faraday_plugins.plugins.manager import PluginsManager
@@ -24,31 +23,32 @@ def main():
         sys.exit()
 
     os.chdir(path_arachni)
-    with tempfile.TemporaryDirectory() as tempdirname:
-        tempdir = Path(tempdirname)
-        name_result = tempdir / 'report.afr'
-        xml_result = tempdir / 'xml_arachni_report.xml'
-        arachni_process = subprocess.Popen([f'./arachni {url_analyze} --report-save-path={name_result}'],
-                                           stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        (out, err) = arachni_process.communicate()
-        if len(out) > 0:
-            print(f"Arachni stdout: {out.decode('utf-8')}", file=sys.stderr)
-        if len(err) > 0:
-            print(f"Arachni stderr: {err.decode('utf-8')}", file=sys.stderr)
+    name_result = Path(path_arachni) / 'report.afr'
+    arachni_command = subprocess.run([f'./arachni {url_analyze} --report-save-path={name_result}'], shell=True,
+                                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        arachni_reporter_process = subprocess.Popen([f'./arachni_reporter {name_result} '
-                                                     f'--reporter=xml:outfile={xml_result}'],
-                                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        (out, err) = arachni_reporter_process.communicate()
-        if len(out) > 0:
-            print(f"Arachni Reporter stdout: {out.decode('utf-8')}", file=sys.stderr)
-        if len(err) > 0:
-            print(f"Arachni Reporter stderr: {err.decode('utf-8')}", file=sys.stderr)
-        plugin = PluginsManager().get_plugin("arachni")
-        plugin.parseOutputString(xml_result)
+    if len(arachni_command.stdout) > 0:
+        print(f"Arachni stdout: {arachni_command.stdout.decode('utf-8')}", file=sys.stderr)
+    if len(arachni_command.stderr) > 0:
+        print(f"Arachni stderr: {arachni_command.stderr.decode('utf-8')}", file=sys.stderr)
+
+    arachni_reporter_process = subprocess.run([f'./arachni_reporter {name_result} '
+                                               f'--reporter=xml:outfile=xml_arachni_report.xml'], shell=True,
+                                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if len(arachni_reporter_process.stdout) > 0:
+        print(f"Arachni stdout: {arachni_reporter_process.stdout.decode('utf-8')}", file=sys.stderr)
+    if len(arachni_reporter_process.stderr) > 0:
+        print(f"Arachni stderr: {arachni_reporter_process.stderr.decode('utf-8')}", file=sys.stderr)
+
+    plugin = PluginsManager().get_plugin("arachni")
+
+    with open("xml_arachni_report.xml", 'r') as f:
+        plugin.parseOutputString(f.read())
         print(plugin.get_json())
+
+    os.remove(name_result)
+    os.remove("xml_arachni_report.xml")
 
 
 if __name__ == '__main__':
     main()
-
