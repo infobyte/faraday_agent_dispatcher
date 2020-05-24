f4c1d8a93f5fc110a5c8742df7af2ac13d52707b
Blas <bmoyano@infobytesec.com>
2020-05-23 23:19:30 -0300

    the zap program must be running

--- faraday_agent_dispatcher/static/executors/official/zap.py
+++ faraday_agent_dispatcher/static/executors/official/zap.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python
 import os
 import sys
-from faraday_plugins.plugins.manager import PluginsManager
 import time
-from zapv2 import ZAPv2
+import subprocess
 import tempfile
+from zapv2 import ZAPv2
+from faraday_plugins.plugins.manager import PluginsManager
 
 
 def main():
     # If the script is run outside the dispatcher the environment variables are checked.
-    # ['API_KEY', 'EXECUTOR_CONFIG_TARGET_URL', 'ZAP_PATH']
+    # ['EXECUTOR_CONFIG_API_KEY', 'EXECUTOR_CONFIG_TARGET_URL']
     try:
         target = os.environ['EXECUTOR_CONFIG_TARGET_URL']
-        api_key = os.environ['API_KEY']
+        api_key = os.environ['EXECUTOR_CONFIG_API_KEY']
     except KeyError:
         print("environment variable not found", file=sys.stderr)
         sys.exit()
 
-    # Chequear que este levantado, sino exitear de manera feliz
+    zap_run = subprocess.check_output('pgrep -f zap', shell=True)
 
-    my_env = os.environ
-    zap = ZAPv2(apikey=api_key)
-    scanID = zap.spider.scan(target)
-    while int(zap.spider.status(scanID)) < 100:
-        time.sleep(1)
+    if len(zap_run.decode('utf-8').split('\n')) > 3:
+        zap = ZAPv2(apikey=api_key)
+        scanID = zap.spider.scan(target)
+        while int(zap.spider.status(scanID)) < 100:
+            time.sleep(1)
 
-    with tempfile.TemporaryFile('w+t') as ft:
-        ft.write(zap.core.xmlreport())
-        zap.core.shutdown()
-        plugin = PluginsManager().get_plugin("zap")
-        plugin.parseOutputString(ft.read())
-        print(plugin.get_json())
+        with tempfile.TemporaryFile('w+t') as f:
+            f.write(zap.core.xmlreport())
+            f.seek(0)
+            plugin = PluginsManager().get_plugin("zap")
+            plugin.parseOutputString(f.read())
+            print(plugin.get_json())
+
+    else:
+        print("ZAP not running", file=sys.stderr)
+        sys.exit()
 
 
 if __name__ == '__main__':
