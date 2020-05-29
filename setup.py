71d01183fca48f2a1402c20076effb1b591c323d
Eric Horvat <eric.nahuel.horvat@gmail.com>
2020-05-29 17:59:04 -0300

    [FIX] All ssl checks. pytest-asyncio [REM] pytest-aiohttp

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open('RELEASE.md') as history_file:
     history = history_file.read()
 
 requirements = ['Click>=6.0', 'websockets', 'aiohttp<4.0.0', 'syslog_rfc5424_formatter',
-                'pytest', 'pytest-aiohttp', 'requests', 'itsdangerous', 'faraday-plugins']
+                'pytest', 'pytest-asyncio', 'requests', 'itsdangerous', 'faraday-plugins']
 
 setup_requirements = ['pytest-runner', 'click', 'setuptools_scm']
 
