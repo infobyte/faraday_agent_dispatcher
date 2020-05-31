7c2e7925963640a43286e0d210385b9a275f328c
Blas <bmoyano@infobytesec.com>
2020-05-31 15:52:59 -0300

    add .md ; create def report

--- faraday_agent_dispatcher/static/executors/official/crackmapexec.py
+++ faraday_agent_dispatcher/static/executors/official/crackmapexec.py
@@ -6,13 +6,35 @@ import socket
 import re
 
 
+def report(output):
+    faraday_host = []
+    for line in output.split("\n"):
+        ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)
+        if ip:
+            info_domain = line.find(":")
+
+            faraday_host_info = {
+                "hosts": [{
+                    "ip": ip[0],
+                    "description": f'{line[info_domain:]}',
+                    "hostnames": None,
+                    "os": "",
+                    "services": None,
+                    "vulnerabilities": None
+                }]}
+
+            faraday_host.append(faraday_host_info)
+        else:
+            pass
+    return faraday_host
+
+
 def main():
     # If the script is run outside the dispatcher the environment variables are checked.
     # ['EXECUTOR_CONFIG_CRACKMAPEXEC_IP', 'EXECUTOR_CONFIG_CRACKMAPEXEC_USER', 'EXECUTOR_CONFIG_CRACKMAPEXEC_PASS']
     ip = os.environ.get('EXECUTOR_CONFIG_CRACKMAPEXEC_IP')
     user = os.environ.get('EXECUTOR_CONFIG_CRACKMAPEXEC_USER')
     passw = os.environ.get('EXECUTOR_CONFIG_CRACKMAPEXEC_PASS')
-    ip = "192.168.0.10"
     if not ip:
         print("IP not provided", file=sys.stderr)
         sys.exit()
@@ -28,44 +50,16 @@ def main():
         command = f'cme smw {ip}/24 -u {user} -p {passw}'
         p = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
         output = p.stdout.decode('utf-8')
-        for line in output.split("\n"):
-            ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)
-            if ip:
-                info_domain = line.find(":")
+        faraday_json = report(output)
 
-                faraday_host_info = {
-                    "hosts": [{
-                        "ip": ip[0],
-                        "description": f'{line[info_domain:]}',
-                        "hostnames": None,
-                        "os": "",
-                        "services": None,
-                        "vulnerabilities": None
-                    }]}
-                print(faraday_host_info)
-            else:
-                pass
+        print(faraday_json)
+
     else:
         command = f'cme smb {ip}/24 -u "" -p "" '
         p = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
         output = p.stdout.decode('utf-8')
-        for line in output.split("\n"):
-            ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)
-            if ip:
-                info_domain = line.find(":")
-
-                faraday_host_info = {
-                    "hosts": [{
-                        "ip": ip[0],
-                        "description": f'{line[info_domain:]}',
-                        "hostnames": None,
-                        "os": "",
-                        "services": None,
-                        "vulnerabilities": None
-                    }]}
-                print(faraday_host_info)
-            else:
-                pass
+        faraday_json = report(output)
+        print(faraday_json)
 
 
 if __name__ == '__main__':
