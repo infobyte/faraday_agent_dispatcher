1f268993e49f4cba1862f1113036e165d2e9695d
Matías Lang <matiasl@infobytesec.com>
2020-06-01 17:55:08 -0300

    Fix OS argument injection in official executors
    nmap, nikto2, wpscan and w3af could be executed with user-controlled
    options (such as --script for nmap).

--- faraday_agent_dispatcher/static/executors/official/nmap.py
+++ faraday_agent_dispatcher/static/executors/official/nmap.py
@@ -7,9 +7,9 @@ from faraday_plugins.plugins.repo.nmap.plugin import NmapPlugin
 cmd = [
     "nmap",
     "-p{}".format(os.environ.get('EXECUTOR_CONFIG_PORT_LIST')),
+    "-oX", "-",
+    "--",
     os.environ.get('EXECUTOR_CONFIG_TARGET'),
-    "-oX",
-    "-",
 ]
 
 
