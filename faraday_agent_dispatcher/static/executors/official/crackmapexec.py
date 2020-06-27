a5be8b16ec01d786d902c5c80bed08d97bd70a6b
Blas <bmoyano@infobytesec.com>
2020-06-27 20:15:50 -0300

    change json cme

--- faraday_agent_dispatcher/static/executors/official/crackmapexec.py
+++ faraday_agent_dispatcher/static/executors/official/crackmapexec.py
@@ -10,31 +10,63 @@ def report(output):
     faraday_host = []
     for line in output.split("\n"):
         ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)
-        if ip:
-            info_domain = line.find(":")
+        ip_port = re.findall(r'[0-9]+(?:\.[0-9]+){3}(?:\:[0-9]+)', line)
+        os = []
+        credential_passw = ''
 
+        if line.find('Unix'):
+            os.append('Unix')
+        elif line.find('Windows'):
+            os.append('Windows')
+
+        if ip_port:
+            sep = ip_port[0].index(':')
+            port = ip_port[0][sep+1:]
+
+        if line.find('(Pwn3d!)'):
+            info_domain = line.find("[+]")
+            credential_passw = f'{line[info_domain:]}'
+
+        if ip:
             faraday_host_info = {
                 "hosts": [{
                     "ip": ip[0],
-                    "description": f'{line[info_domain:]}',
+                    "description":'CrackMapExec',
                     "hostnames": None,
-                    "os": "",
-                    "services": None,
+                    "os": os,
+                    "credentials": [
+                        {
+                            "name": "credential",
+                            "password": credential_passw
+                        }
+                    ],
+                    "services": [
+                        {
+                            "name": "",
+                            "port": int(port),
+                            "protocol": ""
+                        }
+                    ],
                     "vulnerabilities": None
                 }]}
 
             faraday_host.append(faraday_host_info)
         else:
             pass
+
     return faraday_host
 
 
 def main():
-    # If the script is run outside the dispatcher the environment variables are checked.
-    # ['EXECUTOR_CONFIG_CRACKMAPEXEC_IP', 'EXECUTOR_CONFIG_CRACKMAPEXEC_USER', 'EXECUTOR_CONFIG_CRACKMAPEXEC_PASS']
+    # If the script is run outside the dispatcher
+    # the environment variables are checked.
+    # ['EXECUTOR_CONFIG_CRACKMAPEXEC_IP',
+    # 'EXECUTOR_CONFIG_CRACKMAPEXEC_USER',
+    # 'EXECUTOR_CONFIG_CRACKMAPEXEC_PASS']
     ip = os.environ.get('EXECUTOR_CONFIG_CRACKMAPEXEC_IP')
     user = os.environ.get('EXECUTOR_CONFIG_CRACKMAPEXEC_USER')
     passw = os.environ.get('EXECUTOR_CONFIG_CRACKMAPEXEC_PASS')
+
     if not ip:
         print("IP not provided", file=sys.stderr)
         sys.exit()
@@ -63,7 +95,7 @@ def main():
             '-p', "",
         ]
 
-    p = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
+    p = subprocess.run(command, stdout=subprocess.PIPE, shell=False)
     output = p.stdout.decode('utf-8')
     faraday_json = report(output)
     print(faraday_json)
