d39feb6b6023ffb6747231720bcba6b34903da50
Blas <bmoyano@infobytesec.com>
2020-12-14 23:27:32 -0300

    FIX message nuclei stderr, check nuclei_exclude

--- faraday_agent_dispatcher/static/executors/official/nuclei.py
+++ faraday_agent_dispatcher/static/executors/official/nuclei.py
@@ -3,6 +3,7 @@ import sys
 import tempfile
 import subprocess
 from pathlib import Path
+from urllib.parse import urlparse
 from faraday_plugins.plugins.manager import PluginsManager
 
 
@@ -10,18 +11,24 @@ def main():
     # separate the target list with comma
     NUCLEI_TARGET = os.getenv("EXECUTOR_CONFIG_NUCLEI_TARGET")
     # separate the exclude list with comma
-    NUCLEI_EXCLUDE = os.getenv("EXECUTOR_CONFIG_NUCLEI_EXCLUDE")
+    NUCLEI_EXCLUDE = os.getenv("EXECUTOR_CONFIG_NUCLEI_EXCLUDE", None)
     NUCLEI_TEMPLATES = os.getenv("NUCLEI_TEMPLATES")
 
     lista_target = NUCLEI_TARGET.split(",")
-    lista_exclude = NUCLEI_EXCLUDE.split(",")
+    if NUCLEI_EXCLUDE is not None:
+        lista_exclude = NUCLEI_EXCLUDE.split(",")
+
     with tempfile.TemporaryDirectory() as tempdirname:
         name_urls = Path(tempdirname) / "urls.txt"
         name_output = Path(tempdirname) / "output.json"
         if len(lista_target) > 1:
             with open(name_urls, "w") as f:
                 for url in lista_target:
-                    f.write(f"{url}\n")
+                    url_parse = urlparse(url)
+                    if not url_parse.scheme:
+                        f.write(f"http://{url}\n")
+                    else:
+                        f.write(f"{url}\n")
             cmd = [
                 "nuclei",
                 "-l",
@@ -31,10 +38,15 @@ def main():
             ]
 
         else:
+            url_parse = urlparse(NUCLEI_TARGET)
+            if not url_parse.scheme:
+                url = f"http://{NUCLEI_TARGET}"
+            else:
+                url = f"{NUCLEI_TARGET}"
             cmd = [
                 "nuclei",
                 "-target",
-                NUCLEI_TARGET,
+                url,
                 "-t",
                 NUCLEI_TEMPLATES,
             ]
@@ -53,7 +65,7 @@ def main():
             print(f"Nuclei stdout: {nuclei_process.stdout.decode('utf-8')}", file=sys.stderr)
 
         if len(nuclei_process.stderr) > 0:
-            print(f"Nuclei stdout: {nuclei_process.stderr.decode('utf-8')}", file=sys.stderr)
+            print(f"Nuclei stderr: {nuclei_process.stderr.decode('utf-8')}", file=sys.stderr)
 
         plugin = PluginsManager().get_plugin("nuclei")
         plugin.parseOutputString(nuclei_process.stdout.decode("utf-8"))
