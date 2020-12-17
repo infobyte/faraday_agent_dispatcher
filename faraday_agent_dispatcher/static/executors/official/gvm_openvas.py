fe93823a0ed5f7533d0ff3f5b78370218dab4726
Nahuel Alonso <nalonso@infobytesec.com>
2020-12-17 12:42:50 -0300

    formatting and added TLS support

--- faraday_agent_dispatcher/static/executors/official/gvm_openvas.py
+++ faraday_agent_dispatcher/static/executors/official/gvm_openvas.py
@@ -11,17 +11,23 @@ from gvm.transforms import EtreeCheckCommandTransform
 
 
 def main():
-    user = os.environ.get("USER_GVM")
-    passw = os.environ.get("PASSW_GVM")
-    userssh = os.environ.get("USER_SSH")
-    passwssh = os.environ.get("PASSW_SSH")
+    user = os.environ.get("GVM_USER")
+    passw = os.environ.get("GVM_PASSW")
+    userssh = os.environ.get("SSH_USER")
+    passwssh = os.environ.get("SSH_PASSW")
     host = os.environ.get("HOST")
     port = os.environ.get("PORT")
-    connection_type = os.environ.get("CONNECTION_TYPE")
-    scan_url = os.environ.get("EXECUTOR_CONFIG_SCAN_URL")
-    scan_id = os.environ.get("EXECUTOR_CONFIG_SCAN_ID")
-    port_list = os.environ.get("EXECUTOR_CONFIG_PORT_LIST_ID")
     socket = os.environ.get("SOCKET_PATH")
+    tls_certfile = os.environ.get("TLS_CERTFILE_PATH")
+    tls_cafile = os.environ.get("TLS_CAFILE_PATH")
+    tls_keyfile = os.environ.get("TLS_KEYFILE_PATH")
+    tls_passw = os.environ.get("TLS_PKEY_PASSW")
+    connection_type = os.environ.get("CONNECTION_TYPE").lower()
+    scan_url = os.environ.get("EXECUTOR_CONFIG_SCAN_TARGET")
+    # Defaults to: Full and Fast
+    scan_id = os.environ.get("EXECUTOR_CONFIG_SCAN_ID") or "daba56c8-73ec-11df-a475-002264764cea"
+    # Defaults to: All IANA assigned TCP
+    port_list = os.environ.get("EXECUTOR_CONFIG_PORT_LIST_ID") or "33d0cd82-57c6-11e1-8ed1-406186ea4fc5"
 
     # RAW XML FORMAT
     xml_format = "a994b278-1f62-11e1-96ac-406186ea4fc5"
@@ -29,33 +35,22 @@ def main():
     # OPENVAS SCANNER
     scanner = "08b69003-5fc2-4037-a479-93b440211c73"
 
-
     if not user or not passw or not connection_type:
-        print(
-            "Data config ['User', 'Passw', 'Connection_type'] GVM_OpenVas not " "provided",
-            file=sys.stderr,
-        )
+        print("Data config ['User', 'Passw', 'Connection_type'] GVM_OpenVas not provided", file=sys.stderr)
         sys.exit()
 
     if not scan_url:
         print("Scan Url not provided", file=sys.stderr)
         sys.exit()
 
-    valid_connections = ["socket", "ssh", "tls"]
+    valid_connections = ("socket", "ssh", "tls")
     if connection_type not in valid_connections:
         print("Not a valid connection_type, Choose between socket-ssh-tls", file=sys.stderr)
         sys.exit()
 
-    if not scan_id:
-        # Scan_ID Full and fast
-        scan_id = "daba56c8-73ec-11df-a475-002264764cea"
-
-    if not port_list:
-        # Port List: All IANA assigned TCP
-        port_list = "33d0cd82-57c6-11e1-8ed1-406186ea4fc5"
-
     if not socket and connection_type == "socket":
-        socket = '/var/run/gvmd.sock'
+        # Default Socket
+        socket = "/var/run/gvmd.sock"
 
     # CONNECTION
     transform = EtreeCheckCommandTransform()
@@ -64,31 +59,26 @@ def main():
     elif connection_type == "ssh":
         connection = SSHConnection(hostname=host, port=port, username=userssh, password=passwssh)
     elif connection_type == "tls":
-        connection = TLSConnection(hostname=host, port=port)
+        connection = TLSConnection(
+            hostname=host, port=port, certfile=tls_certfile, cafile=tls_cafile, keyfile=tls_keyfile, password=tls_passw
+        )
 
     # Create Target
     with Gmp(connection=connection, transform=transform) as gmp:
         gmp.authenticate(user, passw)
         name = "Suspect Host {} {}".format(scan_url, str(datetime.datetime.now()))
 
-        response = gmp.create_target(
-            name=name, hosts=[scan_url], port_list_id=port_list
-        )
+        response = gmp.create_target(name=name, hosts=[scan_url], port_list_id=port_list)
 
     target_id = response.get("id")
 
     # Create Task
     with Gmp(connection=connection, transform=transform) as gmp:
         gmp.authenticate(user, passw)
-        name = "Scan Suspect Host {}".format(scan_url)
-        response = gmp.create_task(
-            name=name,
-            config_id=scan_id,
-            target_id=target_id,
-            scanner_id=scanner
-        )
+        name = "Scan Suspect Host {} {}".format(scan_url, str(datetime.datetime.now()))
+        response = gmp.create_task(name=name, config_id=scan_id, target_id=target_id, scanner_id=scanner)
 
-    task_id = response.get('id')
+    task_id = response.get("id")
 
     # Start Task
     with Gmp(connection=connection, transform=transform) as gmp:
@@ -98,27 +88,36 @@ def main():
 
     report_id = response[0].text
 
-    def getTask(task_id):
-        with Gmp(connection=connection, transform=transform) as gmp:
-            gmp.authenticate(user, passw)
+    # Check task status function
+    def get_task(_task_id):
+        with Gmp(connection=connection, transform=transform) as _gmp:
+            _gmp.authenticate(user, passw)
 
-            response = gmp.get_task(task_id)
-            status = response[1].find("status").text
+            _response = _gmp.get_task(_task_id)
+            status = _response[1].find("status").text
 
             return True if status == "Done" else False
 
-
     scan_done = False
 
-    while scan_done == False:
-        scan_done = getTask(task_id)
+    # Loop to see if scan is done
+    while not scan_done:
+        scan_done = get_task(task_id)
         time.sleep(5)
 
+    # Get report
     with Gmp(connection=connection, transform=transform) as gmp:
         gmp.authenticate(user, passw)
 
-        report = gmp.get_report(report_id=report_id, report_format_id=xml_format, filter="apply_overrides=0 levels=hml rows=-1 min_qod=70 first=1 sort-reverse=severity notes=0 overrides=0", details=True)
+        report = gmp.get_report(
+            report_id=report_id,
+            report_format_id=xml_format,
+            filter="apply_overrides=0 levels=hml rows=-1 min_qod=70 first=1 sort-reverse=severity "
+            "notes=0 overrides=0",
+            details=True,
+        )
 
+    # Parse report and send to Faraday
     plugin = OpenvasPlugin()
     plugin.parseOutputString(ET.tostring(report[0], encoding="unicode"))
     print(plugin.get_json())
