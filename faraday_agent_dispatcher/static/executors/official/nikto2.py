74e3a0694674cdb991c909b22d20b9a3258eafdf
GMartinez1995 <gmartinez@infobytesec.com>
2021-12-03 15:01:18 -0300

    remove --port parameter to nikto executor

--- faraday_agent_dispatcher/static/executors/official/nikto2.py
+++ faraday_agent_dispatcher/static/executors/official/nikto2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import os
-import re
 import sys
 import subprocess
 import tempfile
@@ -13,9 +12,6 @@ def main():
     # are checked.
     # ['EXECUTOR_CONFIG_TARGET_URL', 'EXECUTOR_CONFIG_TARGET_PORT']
     url_target = os.environ.get("EXECUTOR_CONFIG_TARGET_URL")
-    url_port = os.environ.get("EXECUTOR_CONFIG_TARGET_PORT")
-    FULL_URI = r"((http|https):\/\/(www\.)?)"
-    is_full_uri = re.match(FULL_URI, url_target)
     if not url_target:
         print("URL not provided", file=sys.stderr)
         sys.exit()
@@ -31,11 +27,6 @@ def main():
             "-o",
             name_result,
         ]
-        if url_port and not is_full_uri:
-            cmd += [
-                "-p",
-                url_port,
-            ]
 
         nikto_process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if len(nikto_process.stdout) > 0:
