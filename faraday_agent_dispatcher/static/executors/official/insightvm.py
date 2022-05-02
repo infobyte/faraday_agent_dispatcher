1befc1060ef8b0f97b2f04e86a7fdcd2d1efcf30
GMartinez1995 <gmartinez@infobytesec.com>
2022-05-02 17:00:59 -0300

    Now insightvm scan a site if a site_id is sent

--- faraday_agent_dispatcher/static/executors/official/insightvm.py
+++ faraday_agent_dispatcher/static/executors/official/insightvm.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
+import time
+
 import requests
 from requests.auth import HTTPBasicAuth
 import datetime
@@ -15,11 +17,13 @@ def log(message):
 def main():
     # If the script is run outside the dispatcher
     # the environment variables are checked.
-    # ['TARGET_URL', 'EXECUTIVE_REPORT_ID']
+    # ['INSIGHTVM_HOST', 'INSIGHTVM_USR', 'INSIGHTVM_PASSWD', 'EXECUTOR_CONFIG_SITE_ID'
+    # or 'EXECUTOR_CONFIG_EXECUTIVE_REPORT_ID']
     INSIGHTVM_HOST = os.getenv("INSIGHTVM_HOST")
     INSIGHTVM_USR = os.getenv("INSIGHTVM_USR")
     INSIGHTVM_PASSWD = os.getenv("INSIGHTVM_PASSWD")
     EXECUTIVE_REPORT_ID = os.getenv("EXECUTOR_CONFIG_EXECUTIVE_REPORT_ID")
+    SITE_ID = os.getenv("EXECUTOR_CONFIG_SITE_ID")
     host_re = re.compile(r"^https?://.+:\d+$")
 
     if not INSIGHTVM_USR:
@@ -37,20 +41,110 @@ def main():
     if not host_re.match(INSIGHTVM_HOST):
         log(f"INSIGHTVM_HOST is invalid, must be http(s)://HOST(:PORT) [{INSIGHTVM_HOST}]")
         sys.exit(1)
-    log(f"Fetching from: {INSIGHTVM_HOST}")
-    report_url = f"{INSIGHTVM_HOST}/api/3/reports/{EXECUTIVE_REPORT_ID}/history/latest/output"
-    log(f"Connecting to insightvm on {INSIGHTVM_HOST}")
+
+    if SITE_ID:
+        scan_id = run_scan(INSIGHTVM_USR, INSIGHTVM_PASSWD, INSIGHTVM_HOST, SITE_ID)
+        scan_status = wait_scan(INSIGHTVM_USR, INSIGHTVM_PASSWD, INSIGHTVM_HOST, scan_id)
+        if scan_status == "finished":
+            report_id = create_and_generate_report(INSIGHTVM_USR, INSIGHTVM_PASSWD, INSIGHTVM_HOST, scan_id)
+            report_response_text = get_report(INSIGHTVM_USR, INSIGHTVM_PASSWD, INSIGHTVM_HOST, report_id)
+        else:
+            log(f"Scan ended with status {scan_status}")
+            sys.exit(1)
+    elif EXECUTIVE_REPORT_ID:
+        report_response_text = get_report(INSIGHTVM_USR, INSIGHTVM_PASSWD, INSIGHTVM_HOST, EXECUTIVE_REPORT_ID)
+    else:
+        log("site_id or executive_id is required")
+        sys.exit(1)
+    plugin = NexposeFullPlugin()
+    plugin.parseOutputString(report_response_text)
+    print(plugin.get_json())
+
+
+def get_report(user, passwd, host, report_id):
+    log(f"Fetching from: {host}")
+    report_url = f"{host}/api/3/reports/{report_id}/history/latest/output"
+    log(f"Connecting to insightvm on {host}")
     try:
-        report_response = requests.get(report_url, verify=False, auth=HTTPBasicAuth(INSIGHTVM_USR, INSIGHTVM_PASSWD))
+        report_response = requests.get(report_url, verify=False, auth=HTTPBasicAuth(user, passwd))
         if report_response.status_code != 200:
             log(f"API gets no response. Status code: {report_response.status_code}")
             sys.exit()
     except Exception as e:
-        log(f"ERROR connecting to insightvm api on {INSIGHTVM_HOST} [{e}]")
+        log(f"ERROR connecting to insightvm api on {host} [{e}]")
         sys.exit()
-    plugin = NexposeFullPlugin()
-    plugin.parseOutputString(report_response.text)
-    print(plugin.get_json())
+    return report_response.text
+
+
+def run_scan(user, passwd, host, site_id):
+    start_scan_url = f"{host}/api/3/sites/{site_id}/scans"
+    log("Running new scan")
+    try:
+        scan_response = requests.post(start_scan_url, verify=False, auth=HTTPBasicAuth(user, passwd))
+        if scan_response.status_code != 201:
+            log(f"API gets no response. Status code: {scan_response.status_code}")
+            sys.exit()
+    except Exception as e:
+        log(f"ERROR connecting to insightvm api on {host} [{e}]")
+        sys.exit()
+    return scan_response.json()["id"]
+
+
+def wait_scan(user, passwd, host, scan_id):
+    check_scan_url = f"{host}/api/3/scans/{scan_id}"
+    scan_status = "running"
+    log(f"Waiting scan {scan_id} to finish")
+    while scan_status == "running":
+        try:
+            scan_status_response = requests.get(check_scan_url, verify=False, auth=HTTPBasicAuth(user, passwd))
+            scan_status = scan_status_response.json()["status"]
+            if scan_status_response.status_code != 200:
+                log(f"API gets no response. Status code: {scan_status_response.status_code}")
+                sys.exit()
+        except Exception as e:
+            log(f"ERROR connecting to insightvm api on {host} [{e}]")
+            sys.exit()
+        time.sleep(20)
+    return scan_status
+
+
+def create_and_generate_report(user, passwd, host, scan_id):
+    create_report_url = f"{host}/api/3/reports"
+    body = {"format": "xml-export-v2",
+            "filters": {
+                "severity": "all",
+                "statuses": [
+                    "vulnerable",
+                    "potentially-vulnerable",
+                    "vulnerable-version"
+                ]
+            },
+            "name": f"Report with scan {scan_id}",
+            "scope": {
+                "scan": scan_id}
+            }
+    try:
+        log("Creating the report")
+        report_create_response = requests.post(create_report_url, verify=False, auth=HTTPBasicAuth(user, passwd),
+                                               json=body)
+        if report_create_response.status_code != 201:
+            log(f"Couldnt create report, Status codae {report_create_response.status_code}")
+            sys.exit()
+        else:
+            report_id = report_create_response.json()["id"]
+            log(report_create_response.json())
+            generate_report_url = f"{host}/api/3/reports/{report_id}/generate"
+            report_generate_response = requests.post(generate_report_url, verify=False,
+                                                     auth=HTTPBasicAuth(user, passwd))
+            if report_generate_response.status_code != 200:
+                log(f"Couldnt create report, Status codae {report_generate_response.status_code}")
+                sys.exit()
+            # Wait till the report is generated
+            time.sleep(60)
+            return report_id
+    except Exception as e:
+        log(f"ERROR connecting to insightvm api on {host} [{e}]")
+        sys.exit()
 
 
 if __name__ == "__main__":
