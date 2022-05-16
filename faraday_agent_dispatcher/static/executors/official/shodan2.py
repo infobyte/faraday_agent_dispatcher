4a36fc66ea2a21b4d85a0fe150d904f60dfee182
GMartinez1995 <gmartinez@infobytesec.com>
2022-05-16 16:16:34 -0300

    Add ingore_info and hostname_resolution options

--- faraday_agent_dispatcher/static/executors/official/shodan2.py
+++ faraday_agent_dispatcher/static/executors/official/shodan2.py
@@ -7,12 +7,13 @@ import subprocess
 import tempfile
 from pathlib import Path
 
-from faraday_plugins.plugins.manager import PluginsManager
+from faraday_plugins.plugins.repo.shodan.plugin import ShodanPlugin
 
 
 def main():
+    ignore_info = (os.getenv("AGENT_CONFIG_IGNORE_INFO", False),)
+    hostname_resolution = os.getenv("AGENT_CONFIG_HOSTNAME_RESOLUTION", True)
     shodan_query = os.environ.get("EXECUTOR_CONFIG_SHODAN_QUERY")
-
     with tempfile.TemporaryDirectory() as tempdirname:
         tmpdir = Path(tempdirname)
         tmpfile = tempfile.NamedTemporaryFile(dir=tmpdir, suffix=".json.gz")
@@ -29,7 +30,7 @@ def main():
             print(f"Shodan stdout: {shodan_process.stdout.decode('utf-8')}", file=sys.stderr)
         if len(shodan_process.stderr) > 0:
             print(f"Shodan stderr: {shodan_process.stderr.decode('utf-8')}", file=sys.stderr)
-        plugin = PluginsManager().get_plugin("shodan")
+        plugin = ShodanPlugin(ignore_info=ignore_info, hostname_resolution=hostname_resolution)
         with gzip.open(name_result, "rb") as f:
             plugin.parseOutputString(f.read().decode("utf-8"))
             print(plugin.get_json())
