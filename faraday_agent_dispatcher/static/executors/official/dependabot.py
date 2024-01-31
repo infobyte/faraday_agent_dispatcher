4b64f7ea6b0036da0abb723a55de0aa6416f7acc
Diego Nadares <dnadares@faradaysec.com>
2024-01-31 12:12:01 -0300

    Dependabot initial commit

---
+++ faraday_agent_dispatcher/static/executors/official/dependabot.py
@@ -0,0 +1,18 @@
+import requests
+import os
+
+def main():
+    # DEPENDABOT_URL = os.getenv("EXECUTOR_CONFIG_DEPENDABOT_URL")  # https://api.github.com/repos/OWNER/REPO/dependabot/alerts
+    # DEPENDABOT_TOKEN = os.getenv("EXECUTOR_CONFIG_DEPENDABOT_TOKEN")
+    # DEPENDABOT_OWNER = os.getenv("DEPENDABOT_OWNER")
+    # DEPENDABOT_REPO = os.getenv("DEPENDABOT_REPO")
+    ...
+    # print(DEPENDABOT_URL)
+    # print(DEPENDABOT_TOKEN)
+    # print(DEPENDABOT_OWNER)
+    # print(DEPENDABOT_REPO)
+
+    return [{'test': 'hola'}]
+
+if __name__ == '__main__':
+    main()
