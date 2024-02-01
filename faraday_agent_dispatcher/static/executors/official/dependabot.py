3468cf3f7dec9759c1f6e632d6d7209c2c54183b
dkraus <dkraus@faradaysec.com>
2024-02-01 10:37:50 -0300

    Fix getenv names

--- faraday_agent_dispatcher/static/executors/official/dependabot.py
+++ faraday_agent_dispatcher/static/executors/official/dependabot.py
@@ -4,6 +4,9 @@ from pprint import pprint
 
 import requests
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 host_data = {
     "ip": "192.168.0.{}",
@@ -41,13 +44,15 @@ vuln_web_data = {
 }
 
 def main():
-    DEPENDABOT_OWNER = os.getenv("DEPENDABOT_OWNER")
-    DEPENDABOT_REPO = os.getenv("DEPENDABOT_REPO")
-    DEPENDABOT_TOKEN = os.getenv("EXECUTOR_CONFIG_DEPENDABOT_TOKEN")
+    DEPENDABOT_OWNER = os.getenv("EXECUTOR_CONFIG_DEPENDABOT_OWNER")
+    DEPENDABOT_REPO = os.getenv("EXECUTOR_CONFIG_DEPENDABOT_REPO")
+    DEPENDABOT_TOKEN = os.getenv("DEPENDABOT_TOKEN")
+    logger.info(f"{os.environ}")
 
     # TODO: should validate config?
     dependabot_url = f"https://api.github.com/repos/{DEPENDABOT_OWNER}/{DEPENDABOT_REPO}/dependabot/alerts"
     dependabot_auth = {'Authorization': f"Bearer {DEPENDABOT_TOKEN}"}
+    logger.error(f"{dependabot_url}, {dependabot_auth}")
 
     response = requests.get(dependabot_url, headers=dependabot_auth)
     if response.status_code == http.HTTPStatus.OK:
