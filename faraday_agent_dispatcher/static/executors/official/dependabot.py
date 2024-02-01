5195338be98bacaef45e8472fb6b22c9083ff223
dkraus <dkraus@faradaysec.com>
2024-02-01 18:41:05 -0300

    Add set of hosts

--- faraday_agent_dispatcher/static/executors/official/dependabot.py
+++ faraday_agent_dispatcher/static/executors/official/dependabot.py
@@ -47,71 +47,75 @@ def main():
     DEPENDABOT_OWNER = os.getenv("EXECUTOR_CONFIG_DEPENDABOT_OWNER")
     DEPENDABOT_REPO = os.getenv("EXECUTOR_CONFIG_DEPENDABOT_REPO")
     DEPENDABOT_TOKEN = os.getenv("DEPENDABOT_TOKEN")
-    logger.info(f"{os.environ}")
 
     # TODO: should validate config?
     dependabot_url = f"https://api.github.com/repos/{DEPENDABOT_OWNER}/{DEPENDABOT_REPO}/dependabot/alerts"
     dependabot_auth = {'Authorization': f"Bearer {DEPENDABOT_TOKEN}"}
-    logger.error(f"{dependabot_url}, {dependabot_auth}")
 
     response = requests.get(dependabot_url, headers=dependabot_auth)
+
     if response.status_code == http.HTTPStatus.OK:
         security_events = response.json()
-        host_data = {
-            "ip": None,
-            "description": "",
-            "hostnames": [],
-            "vulnerabilities": [],
-            "tags": []
-        }
-        for security_event in security_events:
-            # print("#"*10)
-            # pprint(security_event)
-            # print("#"*10)
-            # continue
-            host_data['ip'] = security_event['dependency']['manifest_path']
-            vulnerability_data = security_event['security_advisory']
+        hosts_ips = list({security_event['dependency']['manifest_path'] for security_event in security_events})
+        hosts = []
 
-            extended_description = ""
-            if security_event['state'] != 'open':
-                logger.warning(f"Vulnerability {vulnerability_data['number']} already closed...")
-                continue
-            security_vulnerability = vulnerability_data.get('security_vulnerability')
-            if security_vulnerability:
-                first_patched_version = security_vulnerability.get('first_patched_version', 'N/A')
-                first_patched_version_identifier = first_patched_version.get('identifier')
-                package = security_vulnerability.get('package', None)
-                ecosystem = package.get('ecosystem', 'N/A')
-                name = package.get('name', 'N/A')
-                vulnerable_version_range = security_vulnerability.get('vulnerable_version_range', 'N/A')
-                extended_description = f"```\n" \
-                                       f"Package: {name} ({ecosystem})\n" \
-                                       f"Affected versions: {vulnerable_version_range} \n" \
-                                       f"Patched version: {first_patched_version_identifier}\n" \
-                                       f"```"
-            vulnerability = {
-                "name": f"{vulnerability_data['summary']}",
-                "desc": f"{extended_description}\n{vulnerability_data['description']}\n",
-                "severity": f"{vulnerability_data['severity']}",
-                "type": "Vulnerability",
-                "impact": {
-                    "accountability": False,
-                    "availability": False,
-                },
-                "cvss": {'cvss3': vulnerability_data['cvss']['vector_string']},
-                "cwe": [cwe['cwe_id'] for cwe in vulnerability_data['cwes']],
-                "cve": [cve['value'] for cve in vulnerability_data['identifiers']
-                        if cve['type'] == 'CVE'],
-                "refs": [{'name': reference['url'], 'type': 'other'} for reference in vulnerability_data['references']],
-                "status": 'open' if security_event['state'] == 'open' else 'closed',
-                "tags": []
-            }
-            host_data['vulnerabilities'].append(vulnerability)
-            break
-        # print("#" * 10)
-        # pprint(host_data)
-        # print("#"*10)
-        data = {'hosts': [host_data],
+        for ip in hosts_ips:
+            host_vulns = []
+            for security_event in security_events:
+                if security_event['dependency']['manifest_path'] == ip:
+                    vulnerability_data = security_event['security_advisory']
+
+                    extended_description = ""
+                    if security_event['state'] != 'open':
+                        logger.warning(f"Vulnerability {security_event['number']} already closed...")
+                        continue
+                    security_vulnerability = vulnerability_data.get('security_vulnerability')
+
+                    if security_vulnerability:
+                        first_patched_version = security_vulnerability.get('first_patched_version', 'N/A')
+                        first_patched_version_identifier = first_patched_version.get('identifier')
+                        package = security_vulnerability.get('package', None)
+                        ecosystem = package.get('ecosystem', 'N/A')
+                        name = package.get('name', 'N/A')
+                        vulnerable_version_range = security_vulnerability.get('vulnerable_version_range', 'N/A')
+                        extended_description = f"```\n" \
+                                               f"Package: {name} ({ecosystem})\n" \
+                                               f"Affected versions: {vulnerable_version_range} \n" \
+                                               f"Patched version: {first_patched_version_identifier}\n" \
+                                               f"```"
+
+                    vulnerability = {
+                        "name": f"{vulnerability_data['summary']}",
+                        "desc": f"{extended_description}\n{vulnerability_data['description']}\n",
+                        "severity": f"{vulnerability_data['severity']}",
+                        "type": "Vulnerability",
+                        "impact": {
+                            "accountability": False,
+                            "availability": False,
+                        },
+                        "cvss": {'cvss3': vulnerability_data['cvss']['vector_string']},
+                        "cwe": [cwe['cwe_id'] for cwe in vulnerability_data['cwes']],
+                        "cve": [cve['value'] for cve in vulnerability_data['identifiers']
+                                if cve['type'] == 'CVE'],
+                        "refs": [{'name': reference['url'], 'type': 'other'} for reference in
+                                 vulnerability_data['references']],
+                        "status": 'open' if security_event['state'] == 'open' else 'closed',
+                        "tags": []
+                    }
+                    host_vulns.append(vulnerability)
+                    break
+
+            hosts.append(
+                {
+                    "ip": ip,
+                    "description": "",
+                    "hostnames": [],
+                    "vulnerabilities": host_vulns,
+                    "tags": []
+                }
+            )
+
+        data = {'hosts': hosts,
                 "command": {
                     "tool": "dependabot",
                     "command": "dependabot",
@@ -122,6 +126,7 @@ def main():
                     "duration": 16334,
                     "import_source": "report"}
                 }
+        # pprint(data)
         print(json.dumps(data))
 
 
