6efa015426c45c7fdef145b348d3415ffbc5c38a
Adriano Canavero <acanavero@faradaysec.com>
2024-10-01 12:41:07 -0300

    added list support for shodan_query parameter

--- faraday_agent_dispatcher/static/executors/official/shodan2.py
+++ faraday_agent_dispatcher/static/executors/official/shodan2.py
@@ -23,38 +23,39 @@ def main():
     if host_tag:
         host_tag = host_tag.split(",")
     shodan_query = os.environ.get("EXECUTOR_CONFIG_SHODAN_QUERY")
-    with tempfile.TemporaryDirectory() as tempdirname:
-        tmpdir = Path(tempdirname)
-        tmpfile = tempfile.NamedTemporaryFile(dir=tmpdir, suffix=".json.gz")
-        name_result = tmpfile.name
-        cmd = [
-            "shodan",
-            "download",
-            name_result,
-            f'"{shodan_query}"',
-        ]
+    for query in shodan_query:
+        with tempfile.TemporaryDirectory() as tempdirname:
+            tmpdir = Path(tempdirname)
+            tmpfile = tempfile.NamedTemporaryFile(dir=tmpdir, suffix=".json.gz")
+            name_result = tmpfile.name
+            cmd = [
+                "shodan",
+                "download",
+                name_result,
+                f'"{query.strip()}"',
+            ]
 
-        shodan_process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if len(shodan_process.stdout) > 0:
-            print(
-                f"Shodan stdout: {shodan_process.stdout.decode('utf-8')}",
-                file=sys.stderr,
+            shodan_process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if len(shodan_process.stdout) > 0:
+                print(
+                    f"Shodan stdout: {shodan_process.stdout.decode('utf-8')}",
+                    file=sys.stderr,
+                )
+            if len(shodan_process.stderr) > 0:
+                print(
+                    f"Shodan stderr: {shodan_process.stderr.decode('utf-8')}",
+                    file=sys.stderr,
+                )
+            plugin = ShodanPlugin(
+                ignore_info=ignore_info,
+                hostname_resolution=hostname_resolution,
+                host_tag=host_tag,
+                service_tag=service_tag,
+                vuln_tag=vuln_tag,
             )
-        if len(shodan_process.stderr) > 0:
-            print(
-                f"Shodan stderr: {shodan_process.stderr.decode('utf-8')}",
-                file=sys.stderr,
-            )
-        plugin = ShodanPlugin(
-            ignore_info=ignore_info,
-            hostname_resolution=hostname_resolution,
-            host_tag=host_tag,
-            service_tag=service_tag,
-            vuln_tag=vuln_tag,
-        )
-        with gzip.open(name_result, "rb") as f:
-            plugin.parseOutputString(f.read().decode("utf-8"))
-            print(plugin.get_json())
+            with gzip.open(name_result, "rb") as f:
+                plugin.parseOutputString(f.read().decode("utf-8"))
+                print(plugin.get_json())
 
 
 if __name__ == "__main__":
